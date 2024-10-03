e32afc5ebf708ec654ddcfdb50e6b2f0ec0ec874
diegosmp <diiegosmp@gmail.com>
2024-10-02 21:13:01 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express, { NextFunction, Request, Response } from 'express'
 import 'express-async-errors'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import routes from './routes'
 dotenv.config()
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.use('/api', routes)
